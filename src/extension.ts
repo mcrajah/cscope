b2e5c4699a46ff51fdffacf48736c88c2da37e18
Rajah Chandrasekhar <mcrajah@gmail.com>
2016-12-21 18:20:33 -0800

    walking through directory

--- src/extension.ts
+++ src/extension.ts
@@ -8,68 +8,54 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+export function activate(context: vscode.ExtensionContext) 
+{
+    var path = require("path");
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "cscope" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable_create_db = vscode.commands.registerCommand('extension.create_db', () => {
+    let disposable_create_db = vscode.commands.registerCommand('extension.create_db', () => 
+    {
         // The code you place here will be executed every time your command is executed
-        // console.log("Create a file list starting at " + path.join(vscode.workspace.rootPath, "fileList.txt"));
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         create_db(context);
     });
 
     let disposable_look_up_symbol = vscode.commands.registerCommand('extension.look_up_symbol', () => {
         // The code you place here will be executed every time your command is executed
         // console.log("Create a file list starting at " + vscode.workspace.rootPath);
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         look_up_symbol(context);
     });
 
     let disposable_look_up_global_definition = vscode.commands.registerCommand('extension.look_up_global_definition', () => {
         // The code you place here will be executed every time your command is executed
         // console.log("Create a file list starting at " + vscode.workspace.rootPath);
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         look_up_global_definition(context);
     });
 
         let disposable_look_up_called_functions = vscode.commands.registerCommand('extension.look_up_called_functions', () => {
         // The code you place here will be executed every time your command is executed
         // console.log("Create a file list starting at " + vscode.workspace.rootPath);
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         look_up_called_functions(context);
     });
 
         let disposable_look_up_calling_functions = vscode.commands.registerCommand('extension.look_up_calling_functions', () => {
         // The code you place here will be executed every time your command is executed
         // console.log("Create a file list starting at " + vscode.workspace.rootPath);
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         look_up_calling_functions(context);
     });
 
         let disposable_look_up_text_string = vscode.commands.registerCommand('extension.look_up_text_string', () => {
         // The code you place here will be executed every time your command is executed
         // console.log("Create a file list starting at " + vscode.workspace.rootPath);
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         look_up_text_string(context);
     });
 
         let disposable_close_all_cscope_searches= vscode.commands.registerCommand('extension.close_all_cscope_searches', () => {
         // The code you place here will be executed every time your command is executed
         // console.log("Create a file list starting at " + vscode.workspace.rootPath);
-        // Display a message box to the user
-        // vscode.window.showInformationMessage('cscope!');
         close_all_cscope_searches(context);
     });
 
@@ -89,6 +75,39 @@ export function deactivate()
 
 function create_db(context: vscode.ExtensionContext)
 {
+    var fs = require("fs");
+    var path = require("path")
+    let w_dir = vscode.workspace.rootPath;
+    if(!w_dir)
+    {
+         vscode.window.showInformationMessage('A folder needs to be open');
+         process.exit(1);
+         return;
+    }
+    //Read the directory
+    var recur = function(dir) 
+    {
+        fs.readdir(dir,function(err,list)
+        {
+            list.forEach(function(file)
+            {
+                var file2 = path.resolve(dir, file);
+                fs.stat(file2,function(err,stats)
+                {
+                    if(stats.isDirectory()) 
+                    {
+                        recur(file2);
+                    }
+                    else 
+                    {
+                        console.log(file2);
+                    }
+                })
+            })
+        });
+        console.log("Done")
+    };
+    recur(w_dir);
     console.log("In create_db");
 }
 
